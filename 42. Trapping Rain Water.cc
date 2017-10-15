aa6fa06d8e154ef41c112b704f36e0c461e63e16
FrankWork <lzh00776@163.com>
2017-10-15 22:28:39 +0800

    42

---
+++ 42. Trapping Rain Water.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        
+    }
+};
+
+int main(){
+  Solution so;
+  vector<int> nums={0,1,0,2,1,0,1,3,2,1,2,1};// 6
+  int result = so.firstMissingPositive(nums);
+  cout << result << endl << endl;
+  
+}

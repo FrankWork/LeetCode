72d7981f301c20195bc652794cbc588adcbad9d9
FrankWork <lzh00776@163.com>
2017-10-16 22:58:16 +0800

    43

---
+++ 43. Multiply Strings.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    string multiply(string num1, string num2) {
+        
+    }
+};
+
+int main(){
+  Solution so;
+  string result;
+
+  result = so.multiply("0", "0");
+  cout << result << endl << endl;
+}

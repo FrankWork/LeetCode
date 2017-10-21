e027de55a3b3806a27ef3df807b6cec5780d3781
FrankWork <lzh00776@163.com>
2017-10-21 22:41:05 +0800

    44

--- 44. Wildcard Matching.cc
+++ 44. Wildcard Matching.cc
@@ -8,33 +8,39 @@ class Solution {
 public:
     bool isMatch(string s, string p) {
       int i=0,j=0;
-      bool res=false;
+      bool res=true;
 
       while(i<s.size() && j<p.size()){
-        if(s[i]==p[j]){
+        if(s[i]==p[j] || p[j]=='?'){//|| p[j]=='*'
           i++;
           j++;
-        }else{
-          i++;
-          j=0;
+        }else if(p[j]=='*'){
+          if(j<p.size()-1 && s[i]!=p[j+1]){
+            i++;
+          }else{
+            j++;
+          }
+        }
+        else{
+          res=false;
+          break;
         }
       }
-      if(i==s.size() && j==p.size()){res=true;}
       
       return res;
     }
 };
 
-void print(bool value){
-  cout << (value?"true":"false") << endl;
+void print(bool value, bool expect){
+  cout << (value?"true":"false") << " " << (expect?"true":"false") << endl;
 }
 int main(){
   Solution so;
-  print(so.isMatch("aa","a")      );  // false
-  print(so.isMatch("aa","aa")     );  // true
-  print(so.isMatch("aaa","aa")    );  // false
-  print(so.isMatch("aa", "*")     );  // true
-  print(so.isMatch("aa", "a*")    );  // true
-  print(so.isMatch("ab", "?*")    );  // true
-  print(so.isMatch("aab", "c*a*b"));  // false
+  print(so.isMatch("aa","a")      , false); 
+  print(so.isMatch("aa","aa")     , true); 
+  print(so.isMatch("aaa","aa")    , false); 
+  print(so.isMatch("aa", "*")     , true); 
+  print(so.isMatch("aa", "a*")    , true); 
+  print(so.isMatch("ab", "?*")    , true); 
+  print(so.isMatch("aab", "c*a*b"), false); 
 }

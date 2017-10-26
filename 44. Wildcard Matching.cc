46ab773e27aa36326b17f1bc9f666753f897c7f1
FrankWork <lzh00776@163.com>
2017-10-26 11:36:10 +0800

    44

--- 44. Wildcard Matching.cc
+++ 44. Wildcard Matching.cc
@@ -6,29 +6,31 @@ using namespace std;
 
 class Solution {
 public:
-    bool isMatch(const string& s, const string& p, int i, int j, vector<vector<int>>& result){
-      //backtrace &&  brute force
-      if(i==s.size() && j==p.size()){return true;}
-      
-      if(s[i]==p[j] || p[j]=='?'){
-        return isMatch(s, p, i+1,j+1, result);
-      }else if(p[j]=='*'){
-        for(int k=i;k<=s.size();k++){
-          bool res;
-          if(result[k][j+1]==-1){
-            res = isMatch(s,p,k,j+1, result);
-            result[k][j+1] = res?1:0;
-          }else{
-            res = result[k][j+1]==1?true:false;
-          }
-          if(res){return true;}
-        }
-      }
-      return false;
-    }
     bool isMatch(string s, string p) {
-      vector<vector<int>> result(s.size()+1, vector<int>(p.size()+1, -1));
-      return isMatch(s, p, 0, 0, result);
+      int  slen = s.size(), plen = p.size(), i, j, iStar=-1, jStar=-1;
+      for(i=0,j=0 ; i<slen; ++i, ++j)
+      {
+          if(p[j]=='*')
+          { //meet a new '*', update traceback i/j info
+              iStar = i;
+              jStar = j;
+              --i;
+          }
+          else
+          { 
+              if(p[j]!=s[i] && p[j]!='?')
+              {  // mismatch happens
+                  if(iStar >=0)
+                  { // met a '*' before, then do traceback
+                      i = iStar++;
+                      j = jStar;
+                  }
+                  else return false; // otherwise fail
+              }
+          }
+      }
+      while(p[j]=='*') ++j;
+      return j==plen;
     }
 };
 
@@ -37,21 +39,21 @@ void print(bool value, bool expect){
 }
 int main(){
   Solution so;
-  // print(so.isMatch("aa","a")      , false); 
-  // print(so.isMatch("aa","aa")     , true); 
-  // print(so.isMatch("aaa","aa")    , false); 
-  // print(so.isMatch("aaa","")    , false);   
-  // print(so.isMatch("abc","abc")    , true); 
-  // print(so.isMatch("aaa","a?a")    , true); 
-  // print(so.isMatch("ada","a?a")    , true); 
+  print(so.isMatch("aa","a")      , false); 
+  print(so.isMatch("aa","aa")     , true); 
+  print(so.isMatch("aaa","aa")    , false); 
+  print(so.isMatch("aaa","")    , false);   
+  print(so.isMatch("abc","abc")    , true); 
+  print(so.isMatch("aaa","a?a")    , true); 
+  print(so.isMatch("ada","a?a")    , true); 
 
-  // print(so.isMatch("aa", "aa*")    , true); 
-  // print(so.isMatch("aa", "*")     , true); 
-  // print(so.isMatch("aa", "a*")    , true); 
-  // print(so.isMatch("ab", "?*")    , true); 
-  // print(so.isMatch("aab", "c*a*b"), false); 
-  // print(so.isMatch("abc.out", "*.out"), true); 
-  // print(so.isMatch("abc.out", "*.out*"), true); 
+  print(so.isMatch("aa", "aa*")    , true); 
+  print(so.isMatch("aa", "*")     , true); 
+  print(so.isMatch("aa", "a*")    , true); 
+  print(so.isMatch("ab", "?*")    , true); 
+  print(so.isMatch("aab", "c*a*b"), false); 
+  print(so.isMatch("abc.out", "*.out"), true); 
+  print(so.isMatch("abc.out", "*.out*"), true); 
 
   // 10s
   print(so.isMatch("bbbbbbbabbaabbabbbbaaabbabbabaaabbababbbabbbabaaabaab", "b*b*ab**ba*b**b***bba"), false); 

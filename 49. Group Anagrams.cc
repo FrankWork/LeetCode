1bba6128ad42a0d51eb8fc0c7cb196a57957d839
FrankWork <lzh00776@163.com>
2017-10-27 22:34:34 +0800

    49

--- 49. Group Anagrams.cc
+++ 49. Group Anagrams.cc
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
@@ -9,16 +9,11 @@ class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
       vector<vector<string>> res;
-      map<string, vector<string>> resMap;
+      unordered_map<string, vector<string>> resMap;
 
       for(int i=0;i<strs.size();i++){
         string key = strs[i];
         sort(key.begin(), key.end());
-        auto it = resMap.find(key);
-        if(it==resMap.end()){
-          vector<string> buf;
-          resMap[key]=buf;
-        }
         resMap[key].push_back(strs[i]);
       }
 

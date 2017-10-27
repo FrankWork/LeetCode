9175af10538fa84132dc2ad25404122f6498b7d9
FrankWork <lzh00776@163.com>
2017-10-27 11:55:56 +0800

    48

--- 48. Rotate Image.cc
+++ 48. Rotate Image.cc
@@ -6,7 +6,7 @@ using namespace std;
 
 void print(vector<int>& nums){
   for(int i=0;i<nums.size();i++){
-    cout << nums[i]<<" ";
+    cout << nums[i]<<"\t";
   }
   cout << endl;
 }
@@ -20,7 +20,18 @@ void print2d(vector<vector<int>> res){
 class Solution {
 public:
   void rotate(vector<vector<int>>& matrix) {
+    // (i,j)=>(j,n-1-i)
+    int n = matrix.size();
+    vector<vector<int>> copy;
+    for(int i=0;i<n;i++){
+      copy.push_back(matrix[i]);
+    }
     
+    for(int i=0;i<n;i++){
+      for(int j=0;j<n;j++){
+        swap(copy[i][j], matrix[j][n-1-i]);
+      }
+    }
   }
 };
 
@@ -29,39 +40,29 @@ public:
 int main(){
   Solution so;
 
-  vector<int> nums{1,1,2};
-  print2d(so.rotate(nums));
-  // Given input matrix = 
-  // [
-  //   [1,2,3],
-  //   [4,5,6],
-  //   [7,8,9]
-  // ],
-  
-  // rotate the input matrix in-place such that it becomes:
-  // [
+  vector<vector<int>> matrix{
+    {1,2,3},
+    {4,5,6},
+    {7,8,9}
+  };
+  so.rotate(matrix);
+  print2d(matrix);
   //   [7,4,1],
   //   [8,5,2],
   //   [9,6,3]
-  // ]
 
-  vector<int> nums2{3,3,0,3};
-  print2d(so.rotate(nums2));
-  // Given input matrix =
-  // [
-  //   [ 5, 1, 9,11],
-  //   [ 2, 4, 8,10],
-  //   [13, 3, 6, 7],
-  //   [15,14,12,16]
-  // ], 
-  
-  // rotate the input matrix in-place such that it becomes:
-  // [
+  vector<vector<int>> matrix2{
+    { 5, 1, 9,11},
+    { 2, 4, 8,10},
+    {13, 3, 6, 7},
+    {15,14,12,16}
+  };
+  so.rotate(matrix2);
+  print2d(matrix2);
   //   [15,13, 2, 5],
   //   [14, 3, 4, 1],
   //   [12, 6, 8, 9],
   //   [16, 7,10,11]
-  // ]
 
  
 }

30ea344e8377305b6c890b1e6dfe35f172511acb
FrankWork <lzh00776@163.com>
2018-03-22 22:52:04 +0800

    sync

--- nowcoder/地牢逃脱.cc
+++ nowcoder/地牢逃脱.cc
@@ -3,10 +3,32 @@
 
 using namespace std;
 
+int move(int x, int y, vector<vector<int>>& steps, vector<vector<int>>& board, vector<vector<int>>& used){
+    int n=steps.size(), m=steps[0].size();
+    if(x==n-1 && y==m-1){
+        return 1;
+    }
+    int sum=-1;
+    int tx, ty, res;
+    for(int i=0;i<steps.size();++i){
+        tx = x+steps[i][0];
+        ty = y+steps[i][1];
+        if(tx>=0 && tx<n && ty>=0 && ty<m && used[tx][ty]==0 && board[tx][ty]=='.'){
+            used[tx][ty]=1;
+            res = move(tx, ty, steps, board, used);
+            if(res!=-1)
+            used[tx][ty]=0;
+        }
+    }
+    return sum;
+}
+
 int main(){
   int n, m;
   cin >>n>>m;
   vector<vector<char>> board(n, vector<char>(m));
+  vector<vector<char>> used(n, vector<char>(m));
+    
   for(int i=0;i<n;++i){
     for(int j=0;j<m;++j){
       cin>>board[i][j];
@@ -21,5 +43,4 @@ int main(){
     cin>>steps[i][0]>>steps[i][1];
   }
   
-
 }
